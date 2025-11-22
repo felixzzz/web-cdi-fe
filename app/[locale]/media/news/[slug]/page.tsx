6e1e30313cef622ae068895dd07a7d4cab27c6a8
Mochamad Surya Rafliansyah <89367252+rafli21xrplc@users.noreply.github.com>
2025-11-22 18:19:43 +0700

    feat: update metadata

--- app/[locale]/media/news/[slug]/page.tsx
+++ app/[locale]/media/news/[slug]/page.tsx
@@ -2,6 +2,7 @@ import { NewsDetail } from "@/components/features/Media/Details/Detail";
 import { RelatedPosts } from "@/components/features/Media/Details/RelatedPosts";
 import { NavbarThemeTrigger } from "@/components/shared/NavbarThemeTrigger";
 import { mediaService } from "@/services/Media/MediaService";
+import { Metadata } from "next";
 import { getTranslations } from "next-intl/server";
 import { notFound } from "next/navigation";
 
@@ -10,6 +11,62 @@ export type PageProps = {
     slug: string;
     locale: string;
   };
+};
+
+const description =
+  "PT Chandra Daya Investasi Tbk (CDI Group) merupakan bagian dari investasi infrastruktur Chandra Asri Group, penyedia bahan kimia energi dan solusi infrastruktur terkemuka di Asia Tenggara dan ECGO, perusahaan induk yang berfokus pada investasi bisnis ketenagalistrikan di Thailand. Beragam operasi CDI Group mencakup termasuk penyediaan dan pengolahan air, energi, kepelabuhanan & penyimpanan, dan logistik.";
+
+const baseUrl = "https://cdi-be.cmlabs.dev";
+
+export const metadata: Metadata = {
+  title: "Management and Organization Structure | Chandra Daya Investasi",
+  description: description,
+  keywords: [
+    "Chandra Daya Investasi",
+    "CDI",
+    "CDIA",
+    "PT Chandra Daya Investasi Tbk",
+    "CDI Group",
+  ],
+
+  metadataBase: new URL(baseUrl),
+
+  viewport: {
+    width: "device-width",
+    initialScale: 1.0,
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
+  alternates: {
+    canonical: "/contact-us",
+  },
+  icons: {
+    shortcut: "/assets/frontend/favicon.png",
+  },
+
+  openGraph: {
+    title: "Chandra Daya Investasi",
+    description: description,
+    url: "/contact-us",
+    type: "website",
+    siteName: "Chandra Daya Investasi",
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "Chandra Daya Investasi",
+    description: description,
+  },
+
+  other: {
+    "application-url": "https://cdi-be.cmlabs.dev",
+    "preview-url": "https://cdi-be.cmlabs.dev/file-storage",
+    "download-file": "https://cdi-be.cmlabs.dev/file-download",
+    "add-file-preview": "https://cdi-be.cmlabs.dev/file/preview",
+    "add-file-download": "https://cdi-be.cmlabs.dev/file/download",
+  },
 };
 
 export default async function Page({ params }: PageProps) {
