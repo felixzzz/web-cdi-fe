dceff5d2d9301dfe35157ba4395ffdbc5da80f39
mahendradwipurwanto <mahendradwipurwanto@gmail.com>
2026-03-26 09:42:48 +0700

    update

--- services/Investor/ReportServices.ts
+++ services/Investor/ReportServices.ts
@@ -1,5 +1,7 @@
 import { ApiInstitutionResponse, CalendarApiResponse, InvestorReportApiResponse } from "@/types/Investor/Report";
 
+import axios from "axios";
+
 const API_URL = `${process.env.NEXT_PUBLIC_BASE_URL}/utility/investor`;
 const API_URL_FINANCIAL = `${process.env.NEXT_PUBLIC_BASE_URL}/investor/calendar/list`;
 const API_URL_INSTITUTION = `${process.env.NEXT_PUBLIC_BASE_URL}/institutions/list`;
@@ -58,34 +60,36 @@ export async function getInstitutionsData(locale: string): Promise<ApiInstitutio
 
 // method untuk fetch data page financial calendar investor
 async function getFinancialData(
-  locale: string,
-  page: number = 1,
-  type: string = "",
-  year: string | number = ""
+    locale: string,
+    page: number = 1,
+    type: string = "",
+    year: string | number = ""
 ): Promise<CalendarApiResponse> {
-  try {
-    const params = new URLSearchParams();
-    params.append("page", page.toString());
-    if (type) {
-      params.append("type", type);
-    }
-    if (year) {
-      params.append("year", year.toString());
-    }
+    try {
+        const params: Record<string, string> = {
+            page: page.toString(),
+        };
 
-    const res = await fetch(`${API_URL_FINANCIAL}?${params.toString()}`, {
-      method: "GET",
-      headers: { "Content-Type": "application/json", lang: locale },
-      next: { revalidate: 3600 },
-    });
-    if (!res.ok) {
-      throw new Error(`Failed to fetch financial data: ${res.statusText}`);
+        if (type) params.type = type;
+        if (year) params.year = year.toString();
+
+        const res = await axios.get(API_URL_FINANCIAL, {
+            params,
+            headers: {
+                "Content-Type": "application/json",
+                lang: locale,
+            },
+        });
+
+        return res.data;
+    } catch (error: any) {
+        console.error("Error in getFinancialData:", error?.response || error);
+
+        throw new Error(
+            error?.response?.data?.message ||
+            "Could not fetch financial calendar data."
+        );
     }
-    return res.json();
-  } catch (error) {
-    console.error("Error in getFinancialData:", error);
-    throw new Error("Could not fetch financial calendar data.");
-  }
 }
 
 export const reportService = {
