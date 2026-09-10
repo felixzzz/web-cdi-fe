f429df86ba5c3266d98bf21013e09e7cc111c882
Felix <felix@salim.bz>
2026-09-10 22:25:17 +0700

    feat: make sustainability factoid icons optional and ensure consistent aspect ratio

--- components/features/Sustainability/Environment/SustainabilityFacts.tsx
+++ components/features/Sustainability/Environment/SustainabilityFacts.tsx
@@ -15,16 +15,18 @@ export function SustainabilityFacts({ data }: SustainabilityFactsProps) {
         <div className="grid grid-cols-1 gap-6 lg:grid-cols-2">
           {factoidData.map((item, index) => (
             <div key={index} className="flex gap-4 items-start flex-col">
-              <div className="flex shrink-0 w-[48px]">
-                <Image
-                  src={item.icon}
-                  alt={item.title || "Fact icon"}
-                  title={item.title || "Fact icon"}
-                  width={48}
-                  height={48}
-                  className="w-full"
-                />
-              </div>
+              {item.icon ? (
+                <div className="flex shrink-0 w-[48px]">
+                  <Image
+                    src={item.icon}
+                    alt={item.title || "Fact icon"}
+                    title={item.title || "Fact icon"}
+                    width={48}
+                    height={48}
+                    className="w-full h-auto"
+                  />
+                </div>
+              ) : null}
               <div className="flex flex-col gap-4">
                 <div
                   className="max-w-full prose prose-invert prose-base text-sm lg:text-base leading-snug lg:leading-loose text-justify text-neutral-300"
